fc4701443084eb019a901c2ca395345687e4cb62
Technerd345 <yeabnoah5@gmail.com>
2024-02-19 20:23:01 +0300

    deleted:    components/Chat/ChatUpper.js 	new file:   components/Chat/ChatUpper.jsx 	renamed:    components/Login/LoginComp.js -> components/Login/LoginComp.jsx 	renamed:    components/Register/register.js -> components/Register/register.jsx 	renamed:    components/check/check.js -> components/check/check.jsx 	renamed:    components/search/SearchUpper.js -> components/search/SearchUpper.jsx 	modified:   utils/IpAdress.js

---
+++ components/Chat/ChatUpper.jsx
@@ -0,0 +1,114 @@
+import {
+  View,
+  Text,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import logo from "../../assets/images/logo-removebg-preview.png";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+// import PostComponent from "../check/check";
+
+const { height, width } = Dimensions.get("window");
+export default function ChatUpper() {
+  const [exists, setExists] = useState(true);
+
+  return (
+    <View
+      style={{
+        height: height,
+        backgroundColor: "#040a12",
+      }}
+    >
+      {exists && (
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <View style={{ marginTop: height * 0.3 }}>
+            <Text
+              style={{
+                paddingTop: height * 0.016,
+                color: "gray",
+                fontFamily: "poppins",
+                textAlign: "center",
+                fontSize: 45,
+              }}
+            >
+              Talk with Nerdai
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                setExists(false);
+              }}
+              style={{
+                backgroundColor: "#7864f6",
+                marginLeft: width * 0.03,
+                padding: 10,
+                borderRadius: 10,
+                marginTop: 20,
+                paddingLeft: 20,
+              }}
+            >
+              <Text style={{ color: "white", fontFamily: "poppins" }}>
+                Start Talking
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+
+      {!exists && (
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          <TextInput
+            onChangeText={(input) => {
+              // setUserInput(input);
+            }}
+            placeholder="Ask Nerdai ..."
+            placeholderTextColor={"gray"}
+            style={{
+              backgroundColor: "#000000",
+              width: width * 0.7,
+              paddingLeft: width * 0.05,
+              margin: width * 0.06,
+              borderRadius: 5,
+              borderColor: "#7864f6",
+              borderWidth: 0.3,
+              color: "#fff",
+              fontSize: width * 0.043,
+              fontFamily: "poppins",
+            }}
+          />
+          <TouchableOpacity
+            // onPress={handleSubmit}
+            style={{
+              marginTop: width * 0.085,
+              height: height * 0.087,
+              width: width * 0.18,
+              paddingLeft: width * 0.04,
+              marginLeft: width * -0.05,
+            }}
+          >
+            <Ionicons
+              name="send"
+              style={{
+                color: "#7864f6",
+                fontSize: height * 0.035,
+                // backgroundColor: "#040418",
+              }}
+            />
+          </TouchableOpacity>
+          <View>
+            <Text>what is the capital city of Ethiopia</Text>
+            <Text></Text>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+}
